1913a4d68ec2918e2f482890a5cd066783166b7c
chipunderblade <pfjhyyj@qq.com>
2026-02-11 18:09:28 +0800

    feat: add confgis

---
+++ packages/configs/src/forge_configs/__init__.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from forge_configs.task import (
+    CameraConfig,
+    RobotConfig,
+    TaskConfig,
+    SimulatorConfig,
+    TaskRobotConfig,
+    load_task,
+)
+
+__all__ = [
+    "CameraConfig",
+    "RobotConfig",
+    "TaskConfig",
+    "SimulatorConfig",
+    "TaskRobotConfig",
+    "load_task",
+]
