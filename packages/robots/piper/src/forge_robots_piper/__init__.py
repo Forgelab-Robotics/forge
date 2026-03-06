a4d2b19a7e24da7f6893d23d5e8cd3ed18306740
chipunderblade <pfjhyyj@qq.com>
2026-03-06 14:01:46 +0800

    refactor: remove unused code

--- packages/robots/piper/src/forge_robots_piper/__init__.py
+++
@@ -1,4 +0,0 @@
-from forge_robots_piper.driver import PiperDriver
-from forge_robots_piper.robot import PiperRobot
-
-__all__ = ["PiperDriver", "PiperRobot"]
--- packages/simulators/mujoco/src/forge_simulators_mujoco/__init__.py
+++
@@ -1,5 +0,0 @@
-"""MuJoCo simulator for Forge robotics framework."""
-
-from forge_simulators_mujoco.simulator import MuJoCoSimulator
-
-__all__ = ["MuJoCoSimulator"]
